13337f53aaba1447c6e8185336f95c4ae08c4084
victoralm <victoralmlinx@gmail.com>
2021-10-04 05:23:10 -0300

    S02L08: Setting up a Non DI Scenario part 1

---
+++ LectureCode/WazeCredit/Services/MarketForecaster.cs
@@ -0,0 +1,22 @@
+﻿using WazeCredit.Models;
+
+namespace WazeCredit.Services
+{
+    public class MarketForecaster
+    {
+        public MarketResult GetMarketPrediction()
+        {
+            // Call API to do some complex calculations and current stock market forecast
+
+            return new MarketResult
+            {
+                MarketCondition = MarketCondition.StableUp,
+            };
+        }
+    }
+
+    public class MarketResult
+    {
+        public MarketCondition MarketCondition {  get; set;}
+    }
+}
---
+++ LectureCode/WazeCredit/Utility/SD.cs
@@ -0,0 +1,9 @@
+﻿namespace WazeCredit.Utility
+{
+    /// <summary>
+    /// Static Details class
+    /// </summary>
+    public static class SD
+    {
+    }
+}

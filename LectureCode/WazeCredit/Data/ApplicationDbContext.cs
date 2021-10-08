5eb0ad1bd6504ee171a3e6275a3b033ec11f1f8b
victoralm <victoralmlinx@gmail.com>
2021-10-08 10:51:33 -0300

    S05L32: Project Setup P1

--- LectureCode/WazeCredit/Data/ApplicationDbContext.cs
+++ LectureCode/WazeCredit/Data/ApplicationDbContext.cs
@@ -3,11 +3,14 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WazeCredit.Models;
 
 namespace WazeCredit.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<CreditApplication> CreditApplicationModel {  get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {

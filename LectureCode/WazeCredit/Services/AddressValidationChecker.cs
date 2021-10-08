3acb6219395166ccecf2c8da48b81f0598e2f6f0
victoralm <victoralmlinx@gmail.com>
2021-10-08 11:17:50 -0300

    S05L34: Validation Checker Service

---
+++ LectureCode/WazeCredit/Services/AddressValidationChecker.cs
@@ -0,0 +1,19 @@
+﻿using WazeCredit.Models;
+
+namespace WazeCredit.Services
+{
+    public class AddressValidationChecker : IValidationChecker
+    {
+        public string ErrorMessage => "Location validation failed";
+
+        public bool ValidatorLogic(CreditApplication model)
+        {
+            if(model.PostalCode <= 0 || model.PostalCode >= 99999)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LectureCode/WazeCredit/Services/CreditValidationChecker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using WazeCredit.Models;
+
+namespace WazeCredit.Services
+{
+    public class CreditValidationChecker : IValidationChecker
+    {
+        public string ErrorMessage => "You didn't meet Age/Salary/Credit requirements...";
+
+        public bool ValidatorLogic(CreditApplication model)
+        {
+            if(DateTime.Now.AddYears(-18) < model.DOB)
+                return false;
+
+            if(model.Salary < 10000)
+                return false;
+
+            return true;
+        }
+    }
+}

1befb6a58c676b1c2806710f406de1fb656f1e26
victoralm <victoralmlinx@gmail.com>
2021-10-09 15:55:05 -0300

    S05L35: Credit Validator Interface to use multiple services

---
+++ LectureCode/WazeCredit/Services/CreditValidator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WazeCredit.Models;
+
+namespace WazeCredit.Services
+{
+    public class CreditValidator : ICreditValidator
+    {
+        private readonly IEnumerable<IValidationChecker> _validations;
+
+        public CreditValidator(IEnumerable<IValidationChecker> validations)
+        {
+            this._validations = validations;
+        }
+
+        /// <summary>
+        /// Check all validations for the CreditApplication model
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>A boolean indicating that the operation success or fail. A list of string of all the errors if it fails.</returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public async Task<(bool, IEnumerable<string>)> PassAllValidations(CreditApplication model)
+        {
+            bool validationsPassed = true;
+
+            // Used to iterate through all the validation error messages
+            List<string> errorMessages = new List<string>();
+
+            foreach (var validation in this._validations)
+            {
+                if (!validation.ValidatorLogic(model))
+                {
+                    // Error
+                    errorMessages.Add(validation.ErrorMessage);
+                    validationsPassed = false;
+                }
+            }
+
+            return (validationsPassed, errorMessages);
+        }
+    }
+}
---
+++ LectureCode/WazeCredit/Services/ICreditValidator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WazeCredit.Models;
+
+namespace WazeCredit.Services
+{
+    public interface ICreditValidator
+    {
+        Task<(bool, IEnumerable<string>)> PassAllValidations(CreditApplication model);
+    }
+}

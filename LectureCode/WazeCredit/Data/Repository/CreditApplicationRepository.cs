69294aa06de21e3d3a391e921a8c560be8a5cf7a
victoralm <victoralmlinx@gmail.com>
2021-10-09 23:51:35 -0300

    S05L44: Bonus - Implement Repository Pattern

---
+++ LectureCode/WazeCredit/Data/Repository/CreditApplicationRepository.cs
@@ -0,0 +1,22 @@
+﻿using WazeCredit.Data.Repository.IRepository;
+using WazeCredit.Models;
+
+namespace WazeCredit.Data.Repository
+{
+    public class CreditApplicationRepository : Repository<CreditApplication>, ICreditApplicationRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CreditApplicationRepository(ApplicationDbContext db) : base(db)
+        {
+            this._db = db;
+        }
+
+        // Update method is implemented here since isn't a good practice to implement it on the Repository
+
+        public void Update(CreditApplication obj)
+        {
+            this._db.CreditApplicationModel.Update(obj);
+        }
+    }
+}
---
+++ LectureCode/WazeCredit/Data/Repository/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using WazeCredit.Data.Repository.IRepository;
+using WazeCredit.Models;
+
+namespace WazeCredit.Data.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public ICreditApplicationRepository CreditApplicationRepository { get; private set; }
+
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            this._db = db;
+            CreditApplicationRepository = new CreditApplicationRepository(this._db);
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
